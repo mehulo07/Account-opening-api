00a7a43f87f9aae6c0de1b184ac111814b285384
rathomoh <rathomoh@core11.ighpl.com>
2020-05-28 16:40:19 +0530

    GFHG

---
+++ src/main/java/com/bns/model/PaymentDeliveryTerms.java
@@ -0,0 +1,53 @@
+package com.bns.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="account_opening_payment_deli_terms_tab")
+public class PaymentDeliveryTerms implements Serializable{
+
+	/**
+	 * @author rathomoh
+	 */
+	private static final long serialVersionUID = 1L;
+	private long deliveryTermsId;
+	private String deliveryTermsName;
+	public PaymentDeliveryTerms(long deliveryTermsId, String deliveryTermsName) {
+		this.deliveryTermsId = deliveryTermsId;
+		this.deliveryTermsName = deliveryTermsName;
+	}
+	public PaymentDeliveryTerms() {
+		
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "deli_terms_id",nullable = false)
+	public long getDeliveryTermsId() {
+		return deliveryTermsId;
+	}
+	public void setDeliveryTermsId(long deliveryTermsId) {
+		this.deliveryTermsId = deliveryTermsId;
+	}
+	@Column(name = "deli_terms_name",nullable = false)
+	public String getDeliveryTermsName() {
+		return deliveryTermsName;
+	}
+	public void setDeliveryTermsName(String deliveryTermsName) {
+		this.deliveryTermsName = deliveryTermsName;
+	}
+	@Override
+	public String toString() {
+		return "PaymentDeliveryTerms [deliveryTermsId=" + deliveryTermsId + ", deliveryTermsName=" + deliveryTermsName
+				+ "]";
+	}
+	
+	
+
+}
