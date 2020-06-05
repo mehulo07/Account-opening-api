7b665a4de1743c881ebcb1f4b6ec0e791ae875ad
unknown <mehulmakwana339@gmail.com>
2020-06-05 08:53:16 +0530

    commit by hussain

--- src/main/java/com/bns/SpringBootJwtApplication.java
+++ src/main/java/com/bns/SpringBootJwtApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -25,6 +31,31 @@ public class SpringBootJwtApplication extends SpringBootServletInitializer{
     protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
     	return springApplicationBuilder.sources(SpringBootJwtApplication.class);
     }
+    
+    
+    @Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.bns"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Account Opening Service", "Documentation of Account Opening Rest service", "1.0",
+				"Free to use", 
+				new springfox.documentation.service.Contact("Luxsh Technologies LTD.","http://luxshtech.com/", "hussain.doctor@bnscolorama.co.uk"),
+				"API Liceance", 
+				"http://luxshtech.com/index.html", 
+				java.util.Collections.EMPTY_LIST
+				);
+	}
+	
 	
 
 }
